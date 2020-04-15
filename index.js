4eee5fc249122fc000fd6e03e5609463a5ba5b08
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-04-15 06:40:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+saturdayFun("bath")
+function saturdayFun (activity="roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+let mondayWork = function (activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(){
+  return function(characteristic = "special"){
+    return `You are ${characteristic}`
+    return function (name = "*") {
+      
+    }
+  }
+}
+
+let encouragingPromptFunction = wrapAdjective("!!!")
